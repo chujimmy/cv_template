51c8c56a3dc9c22e18d42f2e0e9729f7798df127
Jimmy Chu <jchu@meilleursagents.com>
2021-04-25 23:23:49 +0200

    feat(latexmk): add watcher on cv.tex
    Docker container is automatically launched with latexmk
    latexmk will allow to recompile CVs automically on change

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+find / -name "cv.tex" | while read fname; do
+  latexmk -C -cd $fname
+  # Could use -interaction=batchmode which seems intended for automated scripts?
+  latexmk -pdf -pvc -file-line-error -view=none -interaction=nonstopmode -recorder -cd $fname > /proc/1/fd/1 2>/proc/1/fd/2 &
+done
+
+tail -f /dev/null
