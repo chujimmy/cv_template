b400df72d94ce473679af69de5cd520618db378c
Jimmy Chu <jchu@meilleursagents.com>
2021-04-30 19:26:44 +0200

    ci(actions): add github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-find / -name "cv.tex" | while read fname; do
+find . -name "${FILE_PATTERN}.tex" | while read fname; do
   latexmk -C -cd $fname
   # Could use -interaction=batchmode which seems intended for automated scripts?
   latexmk -pdf -pvc -file-line-error -view=none -interaction=nonstopmode -recorder -cd $fname > /proc/1/fd/1 2>/proc/1/fd/2 &
