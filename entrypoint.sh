d983f87e5fe13b06253b005ffccd4f322335a638
Jimmy Chu <jchu@meilleursagents.com>
2021-06-12 01:24:42 +0200

    feat(cover_letter): introduce cover letter

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,10 @@ find . -regextype sed -regex ".*/cv/lang/[a-z]\{2\}/cv.tex" | while read fname; 
   latexmk -pdf -pvc -file-line-error -view=none -interaction=nonstopmode -recorder -cd $fname > /proc/1/fd/1 2>/proc/1/fd/2 &
 done
 
+find . -regextype sed -regex ".*/letter/lang/[a-z]\{2\}/letter.tex" | while read fname; do
+  latexmk -C -cd $fname
+  # Could use -interaction=batchmode which seems intended for automated scripts?
+  latexmk -pdf -pvc -file-line-error -view=none -interaction=nonstopmode -recorder -cd $fname > /proc/1/fd/1 2>/proc/1/fd/2 &
+done
+
 tail -f /dev/null
